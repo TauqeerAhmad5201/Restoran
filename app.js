13c70e1917861ee43e2d0b190970ae6b87ef7716
tauqeerahmad5201 <hellotauqeer@gmail.com>
2024-04-03 22:30:44 +0530

    added public

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+
+app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.sendFile(__dirname + '/public/html/index.html')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
